8fca547998dc2b91166156e9a27adf80379968ec
XValentavr <iwilly17@gmail.com>
2021-05-21 13:28:43 +0300

    Create threads

--- SQLConnector/Connect.py
+++ SQLConnector/Connect.py
@@ -1,14 +1,25 @@
 import mysql
 from mysql.connector import MySQLConnection, Error
+import mysql.connector
+import boto3
+import os
 
 
 def connect():
-    try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='domains',
-                                       user='root',
-                                       password='root')
+    ENDPOINT = " domain-database.cluster-c4e6le3yokkr.us-east-2.rds.amazonaws.com "
+    PORT = "3306"
+    USR = "admin"
+    REGION = "us-east-2a"
+    DBNAME = "domain_checker_db"
+    os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+    session = boto3.Session(profile_name='default')
+    client = session.client('rds')
 
+    token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)
+
+    try:
+        conn = mysql.connector.connect(host=ENDPOINT, user=USR, passwd=token, port=PORT, database=DBNAME,
+                                       ssl_ca='[full path]rds-combined-ca-bundle.pem')
     except Error as e:
         print('Error:', e)
 
