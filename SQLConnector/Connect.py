62a48e4344b525ec94a4a731b570f90323b9c5ed
XValentavr <iwilly17@gmail.com>
2021-05-21 14:05:52 +0300

    Create threads

--- SQLConnector/Connect.py
+++ SQLConnector/Connect.py
@@ -2,14 +2,7 @@ import mysql
 from mysql.connector import MySQLConnection, Error
 import pymysql
 
-
-def connect():
-    try:
-        conn = pymysql.connect(host='domain-database.cluster-c4e6le3yokkr.us-east-2.rds.amazonaws.com', user='admin',
-                               password='xvalentavr')
-
-    except Error as e:
-        print('Error:', e)
-
-    finally:
-        return conn
+conn = pymysql.connect(host='domain-database.cluster-c4e6le3yokkr.us-east-2.rds.amazonaws.com', port=3306, user='admin',
+                       password='xvalentavr', database='domain_checker_db', charset='utf8mb4')
+cursor = conn.cursor()
+print(cursor)
