3091af0345282f996af7ac2159d8ee272ed9fa52
XValentavr <iwilly17@gmail.com>
2021-05-21 13:54:40 +0300

    Create threads

--- SQLConnector/Connect.py
+++ SQLConnector/Connect.py
@@ -1,21 +1,13 @@
 import mysql
 from mysql.connector import MySQLConnection, Error
-import mysql.connector
-import boto3
-import os
+import pymysql
 
 
 def connect():
-    ENDPOINT = " domain-database.cluster-c4e6le3yokkr.us-east-2.rds.amazonaws.com "
-    PORT = "3306"
-    USR = "admin"
-    REGION = "us-east-2a"
-    DBNAME = "domain_checker_db"
-    os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
-    session = boto3.Session(profile_name='default')
-    client = session.client('rds')
     try:
-        conn = mysql.connector.connect(host=ENDPOINT, user=USR, passwd='xvalentavr', port=PORT, database=DBNAME)
+        conn = pymysql.connect(host='domain-database.cluster-c4e6le3yokkr.us-east-2.rds.amazonaws.com', user='admin',
+                               password='xvalentavr')
+
     except Error as e:
         print('Error:', e)
 
