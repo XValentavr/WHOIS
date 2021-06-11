7ab6e276e8a56bd89a4cea102eab78ff78860a0c
XValentavr <iwilly17@gmail.com>
2021-06-12 00:06:31 +0300

    Create threads

---
+++ SingleDomainGetter.py
@@ -0,0 +1,19 @@
+from GetNetDomains import get_net
+from SQLConnector.EC2Connector import connect
+from SingleDomain.GetSingleDomain import get_single
+
+FINAL_ZONE = '.com'
+
+
+def single_domain():
+    conn = connect()
+    _com = '.com'
+    for link in get_net():
+        link = str(link).replace('[\'', '')
+        link = str(link).replace('\']', '')
+        domain = link + FINAL_ZONE
+        get_single(domain, _com, link, conn)
+
+
+if __name__ == "__main__":
+    single_domain()
